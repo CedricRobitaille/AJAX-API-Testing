78a992f5f3c8a2d62b7a5db4c1f466914665b92f
Cedric <crubbertire@rogers.com>
2025-10-04 01:11:01 -0400

    search function and infinite scroll created

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 const url = "https://pokeapi.co/api/v2/"
 
 const searchButton = document.getElementById("search-button")
+const searchInput = document.getElementById("search-input")
 
+let offset = 0;
+let limit = 18;
+let isLoading = false;
 
 
 
 const errorNotification = (error) => {
+  console.log(error)
+  if (/404/.test(error)) {
+    error = "Error: The Pokemon you're looking for does not exist."
+  }
 
   const body = document.querySelector("body");
   const notification = document.createElement("p")
@@ -23,6 +31,12 @@ const errorNotification = (error) => {
 
 
 
+const clearCards = () => {
+  offset = 0;
+  const resultContainer = document.getElementById("search-results");
+  resultContainer.innerHTML = ""
+}
+
 
 
 
@@ -31,21 +45,29 @@ const errorNotification = (error) => {
  * @param {object} data - Data from a 
  */
 const displayData = async (data) => {
+
   const resultContainer = document.getElementById("search-results");
   data = await data; // Await the data so the func doesn't start without the fetched data
   // resultContainer.innerHTML = ""; // Wipes the container
 
   console.log(data);
 
+
+  // Check if the data contains pokemon or not.
   if (data.id) {
-    makeCard()
+    clearCards();
+    makeCard(data)
   } else {
+    // clearCards();
+    let pokemonTotal = 0;
     for (const pokemon of data.results) {
+      pokemonTotal++;
       const id = pokemon.url.replace("https://pokeapi.co/api/v2/pokemon/", "").slice(0, -1);
-      console.log(id)
 
       pokeData = await fetchData(`pokemon/${id}`);
+      makeCard(pokeData);
     }
+    offset += pokemonTotal
   }
 
 
@@ -53,34 +75,15 @@ const displayData = async (data) => {
 
 
 
-  function makeCard(pokeData) {
-    
-
-  }
-
-  // ===================================
-  // MAKE PLACEHOLDERS
-  // ===================================
-
-  for (const pokemon of data.results) {
+  async function makeCard(pokeData) {
+    // ===================================
+    // MAKE PLACEHOLDERS
+    // ===================================
     const placeholderCard = document.createElement("div")
     placeholderCard.classList.add("card", "placeholder")
-    resultContainer.appendChild(placeholderCard)
-  }
+    resultContainer.appendChild(placeholderCard);
 
-
-
-  // ===================================
-  // GENERATE CARDS
-  // ===================================
-  for (const pokemon of data.results) {
-    const id = pokemon.url.replace("https://pokeapi.co/api/v2/pokemon/", "").slice(0, -1);
-    console.log(id)
-
-    pokeData = await fetchData(`pokemon/${id}`)
-    console.log(pokeData)
-    
-
+    const id = pokeData.id
 
 
     // ===================================
@@ -128,7 +131,6 @@ const displayData = async (data) => {
     // DELETE PLACEHOLDER
     // ===================================
 
-    const placeholderCard = document.querySelector(".placeholder");
     resultContainer.removeChild(placeholderCard);
 
 
@@ -139,22 +141,24 @@ const displayData = async (data) => {
 
     const nextPlaceholder = document.querySelector(".placeholder");
     resultContainer.insertBefore(card, nextPlaceholder);
-    
   }
+  limit = 6;
+  isLoading = false;
 }
 
 
 
 
 /**
- * 
+ * Grabs a data set from the API based on the URL parameters provided
  * @param {*} params 
  * @returns 
  */
 const fetchData = async (params) => {
+  isLoading = true;
   try {
-    const response = await fetch(`${url}/${params}`) // Get the data
-    
+    const response = await fetch(`${url}/${params}?limit=${limit}&offset=${offset}`) // Get the data
+
     if (!response.ok) {
       throw new Error(`HTTP error! Status ${response.status} (${response.statusText})`)
     }
@@ -164,30 +168,32 @@ const fetchData = async (params) => {
     return data;
 
   } catch (error) {
-    errorNotification(error);
-    console.error("Fetch error:", error);
+    isLoading = false;
+    errorNotification(`"Fetch error:" ${error}`);
+    // console.error("Fetch error:", error);
   }
+
 }
 
 
 
 
 
-const search = () => {
-  const searchInput = document.getElementById("search-input")
-
+const search = async () => {
+  console.clear();
   if (searchInput.value) {
-    console.clear();
     console.log(`Searching for: ${searchInput.value}`)
-
     const pokemon = fetchData(`pokemon/${searchInput.value}`)
-    displayData(pokemon)
+
+    if (await pokemon) {
+      displayData(pokemon)
+    }
 
   } else {
-    console.clear();
     console.log("No Value Detected")
+    window.onload = displayData(fetchData("pokemon"));
   }
-  
+
 }
 
 
@@ -195,5 +201,24 @@ const search = () => {
 searchButton.addEventListener("click", () => {
   search();
 });
+
+searchInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    search();
+  }
+});
+
+window.addEventListener("scroll", () => {
+
+  if (!isLoading) {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 50) {
+      displayData(fetchData("pokemon"));
+    }
+  }
+
+
+});
+
+
 console.log(searchButton)
 window.onload = displayData(fetchData("pokemon"));
